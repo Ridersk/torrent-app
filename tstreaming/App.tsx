ca58e2e206bd925ad918a100cedb58f890aa0222
Lucas Maciel Aguiar <lucas.aguiar@br.experian.com>
2023-06-04 18:59:44 -0300

    Trying use webtorrent

--- tstreaming/App.tsx
+++ tstreaming/App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -17,13 +17,38 @@ import {
 } from "react-native";
 
 import {Colors} from "react-native/Libraries/NewAppScreen";
+import WebTorrent from "webtorrent";
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === "dark";
+  const [progress, setProgress] = useState(0);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  useEffect(() => {
+    const client = new WebTorrent({torrentPort: 12345});
+    const magnetLink =
+      "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent";
+
+    console.log("Starting Webtorrent...");
+    client.add(magnetLink, { path: "./downloads" }, function (torrent) {
+      console.log("Webtorrent Callback called");
+      // Download
+      torrent.on("download", function (bytes) {
+        console.log("just downloaded: " + bytes);
+        console.log("total downloaded: " + torrent.downloaded);
+        console.log("download speed: " + torrent.downloadSpeed);
+        console.log("progress: " + torrent.progress);
+        setProgress(torrent.progress);
+      });
+
+      torrent.on("done", function () {
+        console.log("torrent download finished");
+      });
+    });
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -45,7 +70,7 @@ function App(): JSX.Element {
                 color: isDarkMode ? Colors.white : Colors.black,
               },
             ]}>
-            Hello World!!!
+            Progress: {progress}
           </Text>
         </View>
       </ScrollView>
